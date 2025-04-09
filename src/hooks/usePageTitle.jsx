acd9e578a673295fecf13a69a4b2f21110286741
Jon Verne <jon.verne@icloud.com>
2025-04-09 11:42:04 -0700

    set page title to project name

--- src/hooks/usePageTitle.jsx
+++ src/hooks/usePageTitle.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
+import { getProject } from '../services/project'
 
-const getTitle = (pathname) => {
-  switch (pathname) {
-    case '/':
-      return 'Dashboard'
-    case '/projects':
-      return 'Projects'
-    default:
-      return 'Page Prism'
-  }
-}
-
-const usePageTitle = (title) => {
-  const [pageTitle, setPageTitle] = useState(title)
+const usePageTitle = () => {
+  const [pageTitle, setPageTitle] = useState('Page Prism')
   const location = useLocation()
+
   useEffect(() => {
-    setPageTitle(getTitle(location.pathname))
-  }, [location])
+    const fetchTitle = async () => {
+      const { pathname } = location
+      const PROJECT_ID_REGEX = /^\/projects\/([^/]+)$/
+      if (pathname === '/') {
+        setPageTitle('Dashboard')
+      } else if (pathname === '/projects') {
+        setPageTitle('Projects')
+      } else if (PROJECT_ID_REGEX.test(pathname)) {
+        const match = pathname.match(PROJECT_ID_REGEX)
+        const projectId = match?.[1]
+
+        try {
+          const project = await getProject(projectId)
+          setPageTitle(project.name)
+        } catch (err) {
+          setPageTitle('Project Not Found')
+        }
+      } else {
+        setPageTitle('Page Prism')
+      }
+    }
+
+    fetchTitle()
+  }, [location.pathname])
 
   return pageTitle
 }
