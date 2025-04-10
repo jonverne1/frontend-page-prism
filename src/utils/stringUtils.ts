c2a6ab9e9bce9a04a24658ba8dbc8c7b36940ebb
Jon Verne <jon.verne@icloud.com>
2025-04-10 09:12:00 -0700

    Update stringUtils.ts

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -1,6 +1,10 @@
 /**
- * Takes a string as input, then truncates it for display in the UI
+ * Takes a string as input, then truncates it, then adds an ellipsis if it's longer than the max length
+ * @param string - input string to truncate
+ * @param maxLength - max length of the string
+ * @returns A truncated string with an ellipsis if it's greater than maxLength
  */
-export const truncateString = (string: string): string => {
-    return string;
-}
+export const truncateString = (string: string, maxLength: number): string => {
+  if (string.length <= maxLength) return string;
+  return string.slice(0, maxLength - 1) + '…';
+};
